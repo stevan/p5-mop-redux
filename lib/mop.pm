ccfc04677978fc3828e43312e8913fe7d881dcfc
Stevan Little <stevan.little@iinteractive.com>
2013-07-04 13:28:38 -0400

    example of what the MOP should look like written in itself, and some bootstrap goals laid out

--- lib/mop.pm
+++ lib/mop.pm
@@ -61,3 +61,135 @@ sub bootstrap {
 1;
 
 __END__
+
+=pod
+
+=head1 The MOP
+
+  class Object {
+      method new (%args) { ... }
+  
+      method id { ... }
+  
+      method can  ($name)  { ... }
+      method isa  ($class) { ... }
+      method does ($role)  { ... }
+      method DOES ($name)  { ... }
+  
+      method DESTROY { ... }
+  }
+  
+  class Attribute (extends => 'Object') {
+      has $name;
+      has $default;
+      has $storage = {};
+  
+      method name { $name }
+  
+      method key_name { ... }
+  
+      method has_default { ... }
+      method get_default { ... }
+  
+      method storage { $storage }
+  
+      method fetch_data_in_slot_for ($instance) { ... }
+      method store_data_in_slot_for ($instance, $data) { ... }
+      method store_default_in_slot_for ($instance) { ... }
+  }
+  
+  class Method (extends => 'Object') {
+      has $name;
+      has $body;
+  
+      method name { $name }
+      method body { $body }
+  
+      method execute ($invocant, $args) { ... }
+  }
+  
+  role Role {
+      has $name;
+      has $version;
+      has $authority;
+  
+      has $roles            = [];
+      has $attributes       = {};
+      has $methods          = {};
+      has $required_methods = [];
+  
+      method name      { $name }
+      method version   { $version }
+      method authority { $authority }
+  
+      method roles { $roles } 
+  
+      method add_role ($role) { ... }
+      method does_role ($name) { ... }
+  
+      method attribute_class { 'Attribute' }
+  
+      method attributes { $attributes }
+  
+      method add_attribute ($attr) { ... }
+      method get_attribute ($name) { ... }
+      method has_attribute ($name) { ... }
+  
+      method method_class { 'Method' }
+  
+      method methods { $methods }
+  
+      method add_method ($attr) { ... }
+      method get_method ($name) { ... }
+      method has_method ($name) { ... }
+  
+      method required_methods { $required_methods }
+  
+      method add_required_method ($required_method) { ... }
+      method requires_method ($name) { ... }
+  
+      method compose_into ($other) { ... }
+  
+      sub FINALIZE { ... }
+  }
+  
+  class Class (extends => 'Object', with => ['Role']) {
+      has $superclass;
+      has $submethods = {};
+  
+      method superclass { $superclass }
+  
+      method is_abstract { ... }
+  
+      method new_instance { ... }
+  
+      method submethod_class { 'Method' }
+  
+      method submethods { $submethods }
+  
+      method add_submethod ($attr) { ... }
+      method get_submethod ($name) { ... }
+      method has_submethod ($name) { ... }
+  
+      method FINALIZE { ... }
+  }
+  
+=head1 BOOTSTRAPPING GOALS
+
+  Class is an instance of Class
+  Object is an instance of Class
+  Class is a subclass of Object
+
+  Class does Role 
+  Role is instance of Class
+  Role does Role
+
+=cut
+
+
+
+
+
+
+
+
