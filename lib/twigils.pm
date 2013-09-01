a9f029bdce5b572ed39d7e4bc0cbffe818fb1a6d
Florian Ragwitz <rafl@debian.org>
2013-09-01 16:04:34 -0400

    Document some caveats

--- lib/twigils.pm
+++ lib/twigils.pm
@@ -63,4 +63,30 @@ sub _add_allowed_twigil {
     $^H{__PACKAGE__ . '/twigils'} = join '' => $twigil, keys %h;
 }
 
+=head1 CAVEATS
+
+=for :list
+* Only scalars are supported
+This limitation might be removed in the future.
+* Special punctuation variables and alphanumeric infix operators
+Code such as C<$.eq 42> would normally be interpreted as comparing the contents
+of the special variable C<$.> with the constant C<42> using the C<eq> infix
+operator. However, in the presence of a twigil variable who's name consists of a
+special variable name followed by the name of an infix operator (e.g. C<$.eq>)
+an expression like C<$.eq 42> will be interpreted as a lookup of the variable
+C<$.eq> followed by a constant C<42>, which will result in a syntax error. To
+disambiguate between these two possible interpretations, use extra whitespace
+between the special variable and the infix operator, i.e. C<$. eq 42>.
+* Spaces between twigil and the variable identifier are forbidden
+As a consequence of the above caveat, it's not possible to use any whitespace
+between the twigil and the variable identifier, as is possible with perls
+built-in lexical variables: C<$ foo> references the variable C<$foo>. C<$!  foo>
+will most likely cause a compile time error.
+* Long-hand dereferencing syntax is required
+When storing references in twigil variables, the long-hand circumfix
+dereferencing notation has to be used. C<@$!foo> doesn't cause the twigil
+variable C<$!foo> to be dereferenced as an array. C<@{ $!foo }>, however, does.
+
+=cut
+
 1;
