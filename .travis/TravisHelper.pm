91caf040134245ea83eb519f528c78fb60714806
Stevan Little <stevan.little@iinteractive.com>
2013-10-28 07:56:26 -0400

    adding in PWBENNETT/Net-IPAddress-Util to the Travis test

--- .travis/TravisHelper.pm
+++ .travis/TravisHelper.pm
@@ -31,6 +31,7 @@ my @repos = (
     [ 'dams/Action-Retry',                  'experimental/p5-mop' ],
     [ 'stevan/react',                       'master'              ],
     [ 'zakame/hashids.pm',                  'p5-mop'              ],
+    [ 'PWBENNETT/Net-IPAddress-Util',       'master'              ],
 );
 my @dirs = ($mop_repo, (map { $_->[0] } @repos));
 
